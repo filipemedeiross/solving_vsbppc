28bf94135118af69409998d151901f261145d213
Filipe Medeiros <filipemedeiros2013@gmail.com>
2024-07-06 17:39:16 -0300

    Reviewing the "Solution" class

--- src/solution.cpp
+++ src/solution.cpp
@@ -4,79 +4,82 @@
 
 using namespace std;
 
-Solution::Solution (int bins) {
-    n = 0;
-    obj = 0;
-    S = new Bin*[bins];
-}
+Solution::Solution (int bins) : n(0), obj(0), S(new Bin*[bins]) {}
 
-Bin* Solution::operator [] (int bin) {
-    if (bin >= 0 && bin < n)
-        return S[bin];
-    else
-        throw out_of_range("Index out of bounds");
+Bin* Solution::operator [] (int b) {
+    if (b < n)
+        return S[b];
+
+    throw out_of_range("Index out of bounds");
 }
 
 void Solution::copy (Solution& sol) {
-    Bin *bin, *n_bin;
+    Bin *bin, *nbin;
 
-    // Cleaning the bin
+    // Cleaning the bins
     while (n) erase_bin(n-1);
 
     // Copying solution 'sol'
     for (int i = 0; i < sol.n; i++) {
         bin = sol[i];
-        new_bin(bin->k);
 
-        n_bin = S[i];
-        n_bin->s = bin->s;
-        n_bin->n = bin->n;
+        nbin = new_bin(bin->k);
+        nbin->s = bin->s;
+        nbin->n = bin->n;
         for (int item : bin->items)
-            n_bin->items.push_back(item);
+            nbin->items.push_back(item);
     }
 }
 
-void Solution::new_bin (int type) {
-    S[n] = new Bin(type);
+Bin* Solution::new_bin (int t) {
+    Bin* last = S[n] = new Bin(t);
+
     n++;
-    obj += BIN_SIZE[type];
+    obj += BIN_SIZE[t];
+
+    return last;
 }
 
-void Solution::erase_bin (int bin) {
+void Solution::erase_bin (int b) {
+    Bin* bin = S[b];
+
     n--;
-    obj -= BIN_SIZE[S[bin]->k];
+    obj -= BIN_SIZE[bin->k];
 
-    delete S[bin];
-    S[bin] = S[n];
-    S[n]   = nullptr;
+    delete bin;
+
+    if (b != n) {
+        S[b] = S[n];
+        S[n] = nullptr;
+    }
 }
 
-void Solution::alloc (int bin, int type, int item, int size) {
-    int k = S[bin]->k;
+void Solution::alloc (int b, int t, int i, int s) {
+    int k = S[b]->k;
 
-    if (type != k)
-        obj += (BIN_SIZE[type] - BIN_SIZE[k]);
+    if (t != k)
+        obj += BIN_SIZE[t] - BIN_SIZE[k];
 
-    S[bin]->add(item, size, type);
+    S[b]->add(i, s, t);
 }
 
-void Solution::dealloc (int bin, int type, int item, int size) {
-    int k = S[bin]->k;
+void Solution::dealloc (int b, int t, int i, int s) {
+    int k = S[b]->k;
 
-    if (type != k)
-        obj -= (BIN_SIZE[k] - BIN_SIZE[type]);
+    if (t != k)
+        obj -= BIN_SIZE[k] - BIN_SIZE[t];
 
-    S[bin]->remove(item, size, type);
+    S[b]->remove(i, s, t);
 }
 
-void Solution::reinsert (int bin1, int bin2, int type1, int type2, int item, int size) {
-    dealloc(bin1, type1, item, size);
-    alloc(bin2, type2, item, size);
+void Solution::reinsert (int b1, int b2, int t1, int t2, int i, int s) {
+    dealloc(b1, t1, i, s);
+    alloc  (b2, t2, i, s);
 }
 
-void Solution::swap (int bin1, int bin2, int type1, int type2, int item1, int item2, int size1, int size2) {
-    reinsert(bin1, bin2, type1, type2, item1, size1);
-    reinsert(bin2, bin1, type2, type1, item2, size2);
+void Solution::swap (int b1, int b2, int t1, int t2, int i1, int i2, int s1, int s2) {
+    reinsert(b1, b2, t1, t2, i1, s1);
+    reinsert(b2, b1, t2, t1, i2, s2);
 }
 
 void Solution::describe () {
--- src/solution.h
+++ src/solution.h
@@ -5,22 +5,22 @@
 
 class Solution {
     public:
-        int n;    // number of bins
-        int obj;  // objective value of the solution
-        Bin** S;
-        
+        int   n;  // number of bins
+        int obj;  // objective value
+        Bin** S;  // bins reference
+
         Solution (int bins);
 
-        Bin* operator [] (int bin);
+        Bin* operator []  (int b);
         void copy (Solution& sol);
 
-        void new_bin (int type);
-        void erase_bin (int bin);
+        Bin* new_bin   (int t);
+        void erase_bin (int b);
 
-        void alloc (int bin, int type, int item, int size);
-        void dealloc (int bin, int type, int item, int size);
-        void reinsert (int bin1, int bin2, int type1, int type2, int item, int size);
-        void swap (int bin1, int bin2, int type1, int type2, int item1, int item2, int size1, int size2);
+        void alloc    (int b, int t, int i, int s);
+        void dealloc  (int b, int t, int i, int s);
+        void reinsert (int b1, int b2, int t1, int t2, int i, int s);
+        void swap     (int b1, int b2, int t1, int t2, int i1, int i2, int s1, int s2);
 
         void describe ();
 };
