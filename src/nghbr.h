e9b48ca64f0755158ee079d52a1bc91e59228012
Filipe Medeiros <filipemedeiros2013@gmail.com>
2024-07-16 13:41:30 -0300

    Review of neighborhood movements

--- src/nghbr.h
+++ src/nghbr.h
@@ -1,9 +1,11 @@
-#ifndef NEIGHBORHOOD_H
-#define NEIGHBORHOOD_H
+#ifndef NGHBR_H
+#define NGHBR_H
 
 #include "solution.h"
 
 int best_k (int);
+
+int is_feasible (Bin*, int, int*);
 
 int is_feasible (Bin*, Bin*, int, int, int, int, int**);
 
