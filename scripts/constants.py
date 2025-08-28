9e70fbf947c2e124449d987d4e049637d792953b
Filipe Medeiros <filipemedeiros2013@gmail.com>
2025-08-27 21:43:33 -0300

    First version of load data script with errors

---
+++ scripts/constants.py
@@ -0,0 +1,32 @@
+VSBPPC_INSTANCES = [
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith100Items.zip"   ,
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith200Items.zip"   ,
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith500Items_1.zip" ,
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith500Items_2.zip" ,
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith1000Items_1.zip",
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith1000Items_2.zip",
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith1000Items_3.zip",
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith1000Items_4.zip",
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith1000Items_5.zip",
+    "http://faculty.ozyegin.edu.tr/aliekici/files/2021/10/Set1Instanceswith1000Items_6.zip",
+]
+
+
+ITEM_COUNTS = {
+    1 : 100,
+    2 : 200,
+    3 : 500,
+    4 : 1000
+}
+SIZE_INTERVALS = {
+    1 : [1 , 100],
+    2 : [20, 100],
+    3 : [50, 100],
+}
+DENSITY_VALUES = {
+    0  : 0.0,
+    1  : 0.1, 2: 0.2, 3: 0.3, 4: 0.4, 5: 0.5,
+            6: 0.6, 7: 0.7, 8: 0.8, 9: 0.9, 
+    10 : 0.95,
+    11 : 0.99,
+}
