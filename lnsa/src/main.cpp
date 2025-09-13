1936a83c9294bb1ad742013560655bfd152f45a1
Filipe Medeiros <filipemedeiros2013@gmail.com>
2025-09-12 21:02:52 -0300

    Refactoring the lnsa/src/main.cpp, with integrate the all code for lnsa

--- lnsa/src/main.cpp
+++ lnsa/src/main.cpp
@@ -3,68 +3,102 @@
 #include <ctime>
 #include <chrono>
 #include <cstring>
+#include <cstdlib>
 #include "instance.h"
 #include "solution.h"
 #include "greedy.h"
 #include "lnsa.h"
+#include "constants.h"
 
-#define FILE_OUT "out/results_default.txt"
-#define PATIENCE 1000
+#define PATIENCE 150
 #define VERBOSE  0
-#define P        0.25
+#define P        0.25f
 
 using namespace std;
 using namespace std::chrono;
 
+
+int elapsed_seconds(time_point <high_resolution_clock> start) {
+    time_point <high_resolution_clock> stop = high_resolution_clock::now();
+
+    return duration_cast <seconds> (stop - start).count();
+}
+
+
 int main (int argc, char** argv) {
     if (argc < 2) {
-        cout << "Using: ./vsbppc <file_instance> [file_results]" << endl;
-		return -1;
+        cerr << "Using: ./vsbppc <file_instance> [file_results]"      << endl
+             << "Options:"                                            << endl
+             << "  -pa <int>   patience (default " << PATIENCE << ")" << endl
+             << "  -v  <int>   verbose  (default " << VERBOSE  << ")" << endl
+             << "  -p  <float> p        (default " << P        << ")" << endl;
+
+        return -1;
 	}
 
-    srand(static_cast<unsigned>(time(nullptr)));
+    int   patience = PATIENCE;
+    int   verbose  = VERBOSE ;
+    float p        = P;
+    const char* output_file = NULL;
+
+    for (int i = 2; i < argc; i++) {
+        if      (strcmp(argv[i], "-pa") == 0 && i + 1 < argc)
+            patience = atoi(argv[++i]);
+        else if (strcmp(argv[i], "-v" ) == 0 && i + 1 < argc)
+            verbose  = atoi(argv[++i]);
+        else if (strcmp(argv[i], "-p" ) == 0 && i + 1 < argc)
+            p = (float) (atof(argv[++i]));
+        else
+            output_file = argv[i];
+    }
+
+    srand (static_cast <unsigned> (time(nullptr)));
+
+    ofstream write_file;
+    if (output_file) {
+        write_file.open(output_file, ios_base::app);
+
+        if (!write_file.is_open()) {
+            cerr << "Error: could not open output file '" << output_file << "'." << endl;
+            return -1;
+        }
+    }
 
     cout << "Reading the instance..." << endl;
-    Instance instance = Instance (argv[1]);
-    Greedy greedy     = Greedy (&instance);
+    Instance instance (argv[1]);
+    Greedy   greedy   (instance, BIN_TYPES, BIN_SIZE, chooser_greedy2);
 
-    int time;
-    time_point<high_resolution_clock> start, stop;
-    ofstream write_file(argc == 3 ? argv[2] : FILE_OUT, ios_base::app);
+    int time_greedy, time_lnsa;
+    time_point <high_resolution_clock> start;
 
     // Greedy algorithm
-    start = high_resolution_clock::now();
-    Solution *greedy_solution = greedy.initial_solution();
-    stop = high_resolution_clock::now();
-
-    time = duration_cast<seconds>(stop - start).count();
-    write_file << argv[1]              << " " <<
-                  greedy_solution->obj << " " <<
-                  time                 << " ";
+    start       = high_resolution_clock::now();
+    Solution greedy_solution = greedy.initial_solution();
+    time_greedy = elapsed_seconds(start);
 
     // LNSA
-    start = high_resolution_clock::now();
-    Solution *best_solution = lnsa(instance, PATIENCE, VERBOSE, P);
-    stop = high_resolution_clock::now();
+    start     = high_resolution_clock::now();
+    Solution best_solution = lnsa(instance, patience, p, chooser_greedy1, verbose);
+    time_lnsa = elapsed_seconds(start);
 
-    time = duration_cast<seconds>(stop - start).count();
-    write_file << best_solution->obj << " " <<
-                  time               << endl;
+    if (output_file != NULL) {
+        write_file << argv[1]                   << " "
+                   << greedy_solution.get_obj() << " "
+                   << time_greedy               << " "
+                   << best_solution.get_obj()   << " "
+                   << time_lnsa                 << endl;
 
-    // Save the results
-    write_file.close();
+        write_file.close();
+    }
 
     // Print the best solution
     cout << endl;
     cout << "Final solution:" << endl;
-    best_solution->describe();
+    best_solution.describe();
 
     cout << endl;
-    cout << greedy_solution->obj << endl;
-    cout << best_solution->obj   << endl;
-
-    delete greedy_solution;
-    delete best_solution;
+    cout << greedy_solution.get_obj() << endl;
+    cout << best_solution  .get_obj() << endl;
 
     return 0;
 }
